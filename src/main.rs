d54d2ccdf6938baea16cefa587898badc8f97ef1
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-06-01 03:22:43 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,11 @@
 // author: Michael Brockus
 // gmail: <michaelbrockus@gmail.com>
 //
-use bot::Bot;
-use std::env;
+extern crate program;
+use program::foundation;
 
-mod bot;
-
-//
 // main is where program execution starts
-//
-#[tokio::main]
-async fn main() {
-    // We also want to option to store the token in a `.env` file
-    dotenv::dotenv().ok();
-
-    // Grab the bot token from the environment
-    let token = env::var("{{token_variable_name}}").expect("Could not load token from environment");
-
-    // Run the bot. If it errors out, let the user know.
-    if let Err(_) = Bot::run(token.as_str()).await{
-        println!("The bot encountered a catastrophic error. Please contact the bot creator if this happens frequently");
-        return;
-    }
+fn main()
+{
+    foundation();
 } // end of function main
