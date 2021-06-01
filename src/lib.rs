687027d2a563d3b012ab62d1856998d17342437e
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-06-01 11:06:39 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,19 @@
 // author: Michael Brockus
 // gmail: <michaelbrockus@gmail.com>
 //
-pub fun greet() -> &'static str {
+
+//
+// Greet the user
+//
+pub fn greet() -> &'static str {
     "Greetings Rust programmer, let's get started!"
 } // end of function greet
+
+//
+// foundation of the program and related
+// application logic must be implemented
+// in the foundation.
+//
+pub fn foundation() {
+    println!("{message}", message=greet());
+} // end of function foundation
